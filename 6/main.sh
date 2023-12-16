d6d79533657bc9361bd2ca514270c3e78ebb7f9e
blue-pitaya <blue.pitaya@pm.me>
2023-12-16 02:37:45 +0100

    day 6 - bash, part 2

--- 6/main.sh
+++ 6/main.sh
@@ -7,6 +7,8 @@ distance_in=`cat $input_file| tail -n 1| grep -o '[0-9]*'| tr '\n' ' '`
 time=($time_in)
 dist=($distance_in)
 
+# Part 1
+
 result=1
 for (( i=0; i<${#time[@]}; i++ )) ; {
   curr_time=${time[$i]}
@@ -25,5 +27,30 @@ for (( i=0; i<${#time[@]}; i++ )) ; {
 
   result=$(( $result*$ways ))
 }
+
+echo "Result: $result"
+echo ""
+
+# Part 2
+# Could be done with binary search but i wont implement it in bash xd
+
+time=`echo $time_in| sed 's/ //g'`
+dist=`echo $distance_in| sed 's/ //g'`
+
+echo "Time: $time"
+echo "Dist: $dist"
+
+fails=0
+for (( n=0; n<$time; n++ )) ; {
+  d=$(( ($time - $n)*n ))
+
+  if [ $d -le $dist ]; then
+    fails=$(( $fails + 1 ))
+  else
+    break
+  fi
+}
+
+result=$(( $time - (2*$fails) + 1 ))
 
 echo "Result: $result"
