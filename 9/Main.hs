0d3d55cbdd40fd702145f61fe3a2bfef9d400ff1
blue-pitaya <blue.pitaya@pm.me>
2023-12-23 05:21:37 +0100

    day 9 part 2 - haskell

--- 9/Main.hs
+++ 9/Main.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Avoid lambda" #-}
+
 import Data.Char (digitToInt, isDigit)
 import Data.List (isPrefixOf)
 
@@ -15,12 +19,25 @@ getNextNum nums = f [] nums
         lastNums' = last nums : lastNums
         diffs = getDiffs nums
 
+getNextNum2 :: [Int] -> Int
+getNextNum2 nums = f [] nums
+  where
+    f firstNums nums = if all (== 0) nums then foldl (\acc x -> x - acc) 0 firstNums else f firstNums' diffs
+      where
+        firstNums' = head nums : firstNums
+        diffs = getDiffs nums
+
 solve1 :: String -> Int
 solve1 line = getNextNum (map read (words line))
+
+solve2 :: String -> Int
+solve2 line = getNextNum2 (map read (words line))
 
 main :: IO ()
 main = do
   fileContent <- readFile "input.txt"
   let lines' = lines fileContent
   let result = sum (map solve1 lines')
+  let result2 = sum (map solve2 lines')
   print result
+  print result2
